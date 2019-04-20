493a2257e5a23921f40e2a6f36b5e94750b154c5
Firdaus Abd Rahman <firdaus.rahman.777@gmail.com>
2019-04-20 17:39:47 +0800

    adding comments on JS file for future reference.

--- custom.js
+++ custom.js
@@ -10,8 +10,13 @@ function convertToRoman(num) {
   let romanized = '';
 
   for (let i = 0; i < decimalValue.length; i++) {
+    //get the value inside decimalValue (ex: 40>36 -> cannot, 10<36 -> yes)
+    //while the num is lower than decimalValue, it will keep looping
     while (decimalValue[i] <= num) {
+    //get the value inside romanNumeral
+    //ex: X; XX; XXX; XXXV; XXXVI
       romanized += romanNumeral[i];
+    //ex: 36-10->26 ; 26-10->16 ; 16-10->6 ; 6-5->1 ; 1-1->0
       num -= decimalValue[i];
     }
   }
